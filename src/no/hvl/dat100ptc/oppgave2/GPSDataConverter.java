a4f545bfc13a2397249956db38661f578eefdb35
h184924 <184924@stud.hvl.no>
2024-10-14 12:43:32 +0200

    ok

--- src/no/hvl/dat100ptc/oppgave2/GPSDataConverter.java
+++ src/no/hvl/dat100ptc/oppgave2/GPSDataConverter.java
@@ -1,4 +1,5 @@
 package no.hvl.dat100ptc.oppgave2;
+import static java.lang.Integer.*;
 
 import no.hvl.dat100ptc.TODO;
 import no.hvl.dat100ptc.oppgave1.GPSPoint;
@@ -9,12 +10,19 @@ public class GPSDataConverter {
 	private static int TIME_STARTINDEX = 11; 
 
 	public static int toSeconds(String timestr) {
+		String timePart = timestr.split("T")[1].split("\\.")[0];
 		
-		int secs;
-		int hr, min, sec;
+		String timeParts[] = timePart.split(":");
 		
-		// TODO
-		throw new UnsupportedOperationException(TODO.method());
+		
+		int hr = parseInt(timeParts[0]);
+		int min = parseInt(timeParts[1]);
+		int sec = parseInt(timeParts[2]);
+		
+		int secs = (hr/3600) + (min/60) + sec;
+		
+		return secs;
+		
 		
 	}
 
