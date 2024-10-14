4b44cb1b5469f19790f0adf84b0bf3fc5dd1c362
h798204 <havard.mortveit@gmail.com>
2024-10-14 09:59:09 +0200

    Håvard update opg1

--- src/no/hvl/dat100ptc/oppgave1/GPSPoint.java
+++ src/no/hvl/dat100ptc/oppgave1/GPSPoint.java
@@ -4,77 +4,67 @@ import no.hvl.dat100ptc.TODO;
 
 public class GPSPoint {
 	
+	// Objekt variabler 
 	private int time;
 	private double latitude; 
 	private double longitude;
 	private double elevation; 
 
 
-	 
-
-		// TODO - konstruktur
-
-		throw new UnsupportedOperationException(TODO.construtor("GPSPoint"));
-		
+	 // TODO - konstruktur
+	
+	public GPSPoint (int time, double latitude, double longitude, double elevation) {
+		this.time = time; 
+		this.latitude = latitude; 
+		this.longitude = longitude; 
+		this.elevation = elevation; 
 	}
 
+	
+
 	// TODO - get/set metoder
+	
 	public int getTime() {
-		
-		throw new UnsupportedOperationException(TODO.method());
+		return time;
 		
 	}
 
 	public void setTime(int time) {
-				
-		throw new UnsupportedOperationException(TODO.method());
+				this.time = time; 
 		
 	}
 
 	public double getLatitude() {
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+		return latitude; 
 		
 	}
 
 	public void setLatitude(double latitude) {
-		
-		throw new UnsupportedOperationException(TODO.method());
+		this.latitude = latitude; 
 		
 	}
 
 	public double getLongitude() {
-		
-		throw new UnsupportedOperationException(TODO.method());
+		return longitude;
 		
 	}
 
 	public void setLongitude(double longitude) {
-		
-		throw new UnsupportedOperationException(TODO.method());
+		this.longitude = longitude; 
 		
 	}
 
 	public double getElevation() {
-		
-		throw new UnsupportedOperationException(TODO.method());
+		return elevation;
 		
 	}
 
 	public void setElevation(double elevation) {
-		
-		throw new UnsupportedOperationException(TODO.method());
+		this.elevation = elevation;
 		
 	}
 	
 	public String toString() {
-		
-		String str;
-		
-		throw new UnsupportedOperationException(TODO.method());
-
-		// TODO
-		
+		return time + " (" + latitude + "," + longitude + ") " + elevation + "\n";
 	}
 }
