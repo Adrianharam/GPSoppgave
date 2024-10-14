3193ab714baa207f44f19e1097e6e8c36c16a273
h798204 <havard.mortveit@gmail.com>
2024-10-14 12:29:34 +0200

    update Main.java

--- src/no/hvl/dat100ptc/oppgave1/Main.java
+++ src/no/hvl/dat100ptc/oppgave1/Main.java
@@ -2,10 +2,19 @@ package no.hvl.dat100ptc.oppgave1;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
-		// TODO
-		
-	}
-
+    public static void main(String[] args) {
+        
+       
+        GPSPoint point = new GPSPoint(1, 2.0, 3.0, 5.0);
+        
+        
+        System.out.println("Initial time: " + point.getTime());
+        
+   
+        point.setTime(2);
+        
+        
+        System.out.println("Updated GPSPoint: " + point.toString());
+    }
 }
+
