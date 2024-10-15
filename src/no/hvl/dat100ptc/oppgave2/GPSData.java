8be9607aefa73e1dac19771d81df554684221b83
h798204 <havard.mortveit@gmail.com>
2024-10-15 09:20:24 +0200

    bruh

--- src/no/hvl/dat100ptc/oppgave2/GPSData.java
+++ src/no/hvl/dat100ptc/oppgave2/GPSData.java
@@ -1,48 +1,59 @@
 package no.hvl.dat100ptc.oppgave2;
 
-import no.hvl.dat100ptc.TODO;
 import no.hvl.dat100ptc.oppgave1.GPSPoint;
 
 public class GPSData {
 
-	private GPSPoint[] gpspoints;
-	protected int antall = 0;
+    
+    private GPSPoint[] gpspoints;
+    protected int antall = 0;
 
-	public GPSData(int antall) {
+   
+    public GPSData(int n) {
+        gpspoints = new GPSPoint[n]; 
+        antall = 0;                  
+    }
 
-		throw new UnsupportedOperationException(TODO.method());
-		
-		// TODO
-	}
+    
+    public GPSPoint[] getGPSPoints() {
+        return this.gpspoints;
+    }
 
-	public GPSPoint[] getGPSPoints() {
-		return this.gpspoints;
-	}
-	
-	protected boolean insertGPS(GPSPoint gpspoint) {
+    
+    protected boolean insertGPS(GPSPoint gpspoint) {
+        
+        if (antall < gpspoints.length) {
+            gpspoints[antall] = gpspoint; 
+            antall++; 
+            return true; 
+        } else {
+            return false; 
+        }
+    }
 
-		boolean inserted = false;
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
-		// TODO 
-	
-	}
+   
+    public boolean insert(String time, String latitude, String longitude, String elevation) {
+    
+        int timeInt = Integer.parseInt(time);
+        double latDouble = Double.parseDouble(latitude);
+        double longDouble = Double.parseDouble(longitude);
+        double elevDouble = Double.parseDouble(elevation);
 
-	public boolean insert(String time, String latitude, String longitude, String elevation) {
+      
+        GPSPoint gpspoint = new GPSPoint(timeInt, latDouble, longDouble, elevDouble);
 
-		GPSPoint gpspoint;
+        
+        return insertGPS(gpspoint);
+    }
 
-		throw new UnsupportedOperationException(TODO.method());
-
-		// TODO 
-		
-	}
-
-	public void print() {
-
-		throw new UnsupportedOperationException(TODO.method());
-
-		// TODO 
-	}
+   
+    public void print() {
+        System.out.println("====== GPS Data - START ======");
+        for (int i = 0; i < antall; i++) {
+            System.out.print(gpspoints[i].toString());
+        }
+        System.out.println("====== GPS Data - SLUTT ======");
+    }
 }
+
+
--- src/no/hvl/dat100ptc/oppgave2/Main.java
+++ src/no/hvl/dat100ptc/oppgave2/Main.java
@@ -4,10 +4,20 @@ import no.hvl.dat100ptc.oppgave1.GPSPoint;
 
 public class Main {
 
-	
-	public static void main(String[] args) {
-
-		// TODO
-		
-	}
+    public static void main(String[] args) {
+        
+        // Step 1: Create two GPSPoint objects
+        GPSPoint point1 = new GPSPoint(1, 60.385390, 5.332200, 50.0); // Time = 1, latitude, longitude, elevation
+        GPSPoint point2 = new GPSPoint(2, 60.385490, 5.332300, 60.0); // Time = 2, different lat, long, elev
+        
+        // Step 2: Create a GPSData object that can hold 2 GPSPoint objects
+        GPSData gpsData = new GPSData(2);
+        
+        // Step 3: Insert the two GPSPoint objects into the GPSData object using insertGPS method
+        gpsData.insertGPS(point1);
+        gpsData.insertGPS(point2);
+        
+        // Step 4: Print the information of the GPSPoint objects using the print method
+        gpsData.print();  // This will print the data of the GPSPoints
+    }
 }
